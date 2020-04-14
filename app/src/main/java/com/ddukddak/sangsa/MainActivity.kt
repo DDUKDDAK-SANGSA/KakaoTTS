15c38079fd9a67166a01bbd93940c6e9e6cdd798
Seyoung Choi <tpdude0601@gmail.com>
2020-04-14 09:58:05 +0900

    오타 수정 Oops

--- app/src/main/java/com/ddukddak/sangsa/MainActivity.kt
+++ app/src/main/java/com/ddukddak/sangsa/MainActivity.kt
@@ -1,32 +1,11 @@
 package com.ddukddak.sangsa
 
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
-import android.content.IntentFilter
-import android.content.pm.PackageInfo
-import android.content.pm.PackageManager
 import android.os.Bundle
-import android.text.TextUtils
-import android.text.method.ScrollingMovementMethod
 import android.transition.AutoTransition
 import android.transition.TransitionManager
-import android.util.Base64
-import android.util.Log
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.app.NotificationManagerCompat
-import androidx.core.view.isVisible
-import com.github.angads25.toggle.widget.LabeledSwitch
-import com.skydoves.expandablelayout.expandableLayout
-import kotlinx.android.synthetic.main.activitiy_main_2.*
-import kotlinx.android.synthetic.main.activity_main.*
-import java.security.MessageDigest
-import java.security.NoSuchAlgorithmException
+import kotlinx.android.synthetic.main.activity_main_2.*
 
 class MainActivity : AppCompatActivity() {
     var initalStatus: Boolean = false
@@ -34,7 +13,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activitiy_main_2)
+        setContentView(R.layout.activity_main_2)
 
         expandable.parentLayout.setOnClickListener {
             if (expandable.isExpanded) {
